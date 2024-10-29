75b396e3bc2b76b3fc7459ce315a959d3e4dde0c
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-10-28 21:55:53 -0400

    please work

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+
+export default async function Page( ) {
+
+const prisma = new PrismaClient()
+let videos = await prisma.video.findMany()
+
+const videoList = videos.map((video) =>
+<li>{video.name}</li> )
+
+
+return( <>
+<h4>Videos</h4>
+{videoList}
+</> )
+}
 
